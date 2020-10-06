982b84ce701d72e2fcb33b6b368323dbfd701d8f
Mika Tuupola <tuupola@appelsiini.net>
2020-10-06 22:59:31 +0300

    Add time and date to the display

--- main/main.c
+++ main/main.c
@@ -26,28 +26,30 @@ SOFTWARE.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 
+#include <esp_sntp.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <esp_log.h>
 #include <driver/spi_master.h>
-// #include <mipi_dcs.h>
-// #include <mipi_display.h>
-// #include <copepod.h>
-// #include <copepod_hal.h>
-// #include <fps.h>
-// #include <font8x8.h>
-// #include <rgb565.h>
+#include <mipi_dcs.h>
+#include <mipi_display.h>
+#include <hagl.h>
+#include <hagl_hal.h>
+#include <fps.h>
+#include <rgb565.h>
 #include <i2c_helper.h>
 #include <axp202.h>
 #include <pcf8563.h>
 
+#include "font8x13.h"
 #include "helpers/wifi.h"
 #include "helpers/nvs.h"
 #include "sdkconfig.h"
 
 static const char *TAG = "main";
-// static float fb_fps;
+static float fb_fps;
 
 pcf8563_t pcf;
 axp202_t axp;
@@ -60,47 +62,49 @@ Cap refresh rate to 45fps.
 T = 1000 / 45 / (1000 / CONFIG_FREERTOS_HZ)
 
 */
-// void backbuffer_task(void *params)
-// {
-//     TickType_t last;
-//     const TickType_t period = 1000 / 45 / portTICK_RATE_MS;
+void backbuffer_task(void *params)
+{
+    TickType_t last;
+    const TickType_t period = 1000 / 45 / portTICK_RATE_MS;
 
-//     last = xTaskGetTickCount();
+    last = xTaskGetTickCount();
 
-//     while (1) {
-//         pod_flush();
-//         fb_fps = fps();
-//         vTaskDelayUntil(&last, period);
-//     }
+    while (1) {
+        hagl_flush();
+        fb_fps = fps();
+        // vTaskDelayUntil(&last, period);
+    }
 
-//     vTaskDelete(NULL);
-// }
+    vTaskDelete(NULL);
+}
 
 void rtc_task(void *params)
 {
-    // uint16_t color = rgb565(0, 255, 0);
-    // char message[128];
+    uint16_t color = rgb565(0, 255, 0);
+    char16_t message[128];
 
     /* Calculate tm_yday for the first run. */
     mktime(&rtc);
 
     while (1) {
         pcf8563_read(&pcf, &rtc);
-        // sprintf(
-        //     message,
-        //     "%04d-%02d-%02d",
-        //     rtc.tm_year + 1900, rtc.tm_mon + 1, rtc.tm_mday
-        // );
-        ESP_LOGI(TAG, "%04d-%02d-%02d", rtc.tm_year + 1900, rtc.tm_mon + 1, rtc.tm_mday);
-        // pod_put_text(message, 40, 28, color, font8x8);
+        swprintf(
+            message,
+            sizeof(message),
+            L"%04d-%02d-%02d",
+            rtc.tm_year + 1900, rtc.tm_mon + 1, rtc.tm_mday
+        );
+        hagl_put_text(message, 80, 100, color, font8x13);
+        // ESP_LOGI(TAG, "%04d-%02d-%02d", rtc.tm_year + 1900, rtc.tm_mon + 1, rtc.tm_mday);
 
-        // sprintf(
-        //     message,
-        //     "%02d:%02d:%02d",
-        //     rtc.tm_hour, rtc.tm_min, rtc.tm_sec
-        // );
-        ESP_LOGI(TAG, "%02d:%02d:%02d", rtc.tm_hour, rtc.tm_min, rtc.tm_sec);
-        // pod_put_text(message, 48, 38, color, font8x8);
+        swprintf(
+            message,
+            sizeof(message),
+            L"%02d:%02d:%02d",
+            rtc.tm_hour, rtc.tm_min, rtc.tm_sec
+        );
+        hagl_put_text(message, 88, 115, color, font8x13);
+        // ESP_LOGI(TAG, "%02d:%02d:%02d", rtc.tm_hour, rtc.tm_min, rtc.tm_sec);
 
         vTaskDelay(1000 / portTICK_RATE_MS);
     }
@@ -108,13 +112,13 @@ void rtc_task(void *params)
     vTaskDelete(NULL);
 }
 
-// void log_task(void *params)
-// {
+void log_task(void *params)
+{
 //     float vacin, iacin, vvbus, ivbus, vts, temp, pbat, vbat, icharge, idischarge, vaps, cbat;
 //     uint8_t power, charge;
 //     char buffer[128];
 
-//     while (1) {
+    while (1) {
 //         axp192_read(&axp, AXP192_ACIN_VOLTAGE, &vacin);
 //         axp192_read(&axp, AXP192_ACIN_CURRENT, &iacin);
 //         axp192_read(&axp, AXP192_VBUS_VOLTAGE, &vvbus);
@@ -145,12 +149,22 @@ void rtc_task(void *params)
 //         strftime(buffer, 128 ,"%c (day %j)" , &rtc);
 //         ESP_LOGI(TAG, "RTC: %s", buffer);
 
-//         vTaskDelay(5000 / portTICK_RATE_MS);
+        vTaskDelay(5000 / portTICK_RATE_MS);
 
-//         ESP_LOGI(TAG, "fps: %.1f", fb_fps);
-//     }
-//     vTaskDelete(NULL);
-// }
+        ESP_LOGI(TAG, "fps: %.1f", fb_fps);
+    }
+    vTaskDelete(NULL);
+}
+
+static void sntp_set_rtc(struct timeval *tv)
+{
+    struct tm *time;
+
+    ESP_LOGI(TAG, "Got SNTP response, setting RTC.");
+
+    time = localtime(&tv->tv_sec);
+    pcf8563_write(&pcf, time);
+}
 
 void app_main()
 {
@@ -170,27 +184,15 @@ void app_main()
     axp202_init(&axp);
     // axp192_ioctl(&axp, AXP192_COULOMB_COUNTER_ENABLE, NULL);
     // axp192_ioctl(&axp, AXP192_COULOMB_COUNTER_CLEAR, NULL);
-    float vacin;
-    axp202_read(&axp, AXP202_DCDC23_LDO234_EXTEN_CONTROL, &vacin);
 
     ESP_LOGI(TAG, "Initializing PCF8563");
     pcf.read = &i2c_read;
     pcf.write = &i2c_write;
     pcf.handle = &i2c_port;
 
-    // rtc.tm_year = 2020 - 1900;
-    rtc.tm_year = 1999 - 1900;
-    rtc.tm_mon = 12 - 1;
-    rtc.tm_mday = 31;
-    rtc.tm_hour = 23;
-    rtc.tm_min = 59;
-    rtc.tm_sec = 45;
-
-    pcf8563_init(&pcf);
-    pcf8563_write(&pcf, &rtc);
-
-    // ESP_LOGI(TAG, "Initializing display");
-    // pod_init();
+    ESP_LOGI(TAG, "Initializing display");
+    hagl_init();
+    hagl_clear_screen();
 
     ESP_LOGI(TAG, "Initializing non volatile storage");
     nvs_init();
@@ -198,9 +200,16 @@ void app_main()
     ESP_LOGI(TAG, "Initializing wifi");
     wifi_init();
 
+    ESP_LOGI(TAG, "Start SNTP sync");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+    sntp_set_time_sync_notification_cb(sntp_set_rtc);
+    sntp_init();
+
     ESP_LOGI(TAG, "Heap after init: %d", esp_get_free_heap_size());
 
     xTaskCreatePinnedToCore(rtc_task, "RTC", 2048, NULL, 1, NULL, 1);
-    // xTaskCreatePinnedToCore(log_task, "Log", 4096, NULL, 2, NULL, 1);
-    // xTaskCreatePinnedToCore(backbuffer_task, "Backbuffer", 8192, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(log_task, "Log", 4096, NULL, 2, NULL, 1);
+    xTaskCreatePinnedToCore(backbuffer_task, "Backbuffer", 8192, NULL, 1, NULL, 0);
 }
